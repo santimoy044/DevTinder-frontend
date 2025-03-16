5f14faf7a47c29f371790bcbcddeb8ac3bdbc597
santimoy044 <santimoyrana044@gmail.com>
2025-03-17 01:31:42 +0530

    add axios for API calls; update Login component with form handling and styling; remove unused assets and CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+
 import NavBar from  './NavBar' 
 import { BrowserRouter, Routes, Route } from    'react-router-dom'
 import Body from './Body'
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [passWord, setpassWord] = useState("");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:7777/login", {
+        email,
+        passWord,
+      },{withCredentials : true});
+
+      console.log("Login successful:", res.data); // Log the response
+
+    } catch (error) {
+      console.log("Login failed:", error);
+    }
+  };
+
+  return (
+    <div className='flex justify-center items-center my-10'>
+      <div className="card bg-primary text-primary-content w-96">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email ID</legend>
+              <input 
+                type="text" 
+                className="input" 
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">passWord</legend>
+              <input 
+                type="passWord"  // Use "passWord" instead of "text"
+                className="input"  
+                value={passWord}
+                onChange={(e) => setpassWord(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center">
+            <button className="btn" onClick={handleLogin}>Login</button> 
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
